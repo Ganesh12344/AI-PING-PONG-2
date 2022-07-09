77a38b7413a5c24c9bb37ef763251fb559a6b4c4
Ganesh12344 <82883868+Ganesh12344@users.noreply.github.com>
2022-07-09 13:58:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function setup(){
+  var canvas = createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+
+
